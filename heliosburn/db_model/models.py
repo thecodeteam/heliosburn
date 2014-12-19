d79370b7ee23ea8e2cc4637fe537a716dda7bdfc
Adrian Moreno <adrian@morenomartinez.com>
2014-12-19 14:51:42 +0000

    updated db model

--- heliosburn/db_model/models.py
+++ heliosburn/db_model/models.py
@@ -11,10 +11,10 @@ class HttpRequest(Base):
     __tablename__ = "http_request"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime)
-    http_protocol = Column(String)
-    method = Column(String)
-    url = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    http_protocol = Column(String, nullable=False)
+    method = Column(String, nullable=False)
+    url = Column(String, nullable=False)
     response = Column(Integer, ForeignKey('http_response.id'), nullable=True)
 
 
@@ -30,10 +30,10 @@ class HttpResponse(Base):
     __tablename__ = "http_response"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime)
-    http_protocol = Column(String)
-    status_code = Column(Integer)
-    status_description = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    http_protocol = Column(String, nullable=False)
+    status_code = Column(Integer, nullable=False)
+    status_description = Column(String, nullable=False)
 
 
 class HttpResponseHeaders(Base):
@@ -48,7 +48,7 @@ class HttpHeader(Base):
     __tablename__ = "http_header"
 
     id = Column(Integer, primary_key=True)
-    key = Column(String)
+    key = Column(String, nullable=False)
     value = Column(String)
 
 
@@ -56,46 +56,47 @@ class Session(Base):
     __tablename__ = "session"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
+    name = Column(String, nullable=False)
+    description = Column(String, default='')
     test_plan = Column(Integer, ForeignKey('test_plan.id'))
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    started_at = Column(DateTime)
+    stopped_at = Column(DateTime)
+
+
+class SessionTraffic(Base):
+    __tablename__ = "session_traffic"
+
+    id = Column(Integer, primary_key=True)
+    session = Column(Integer, ForeignKey('session.id'), nullable=False)
+    http_request = Column(Integer, ForeignKey('http_request.id'), nullable=False)
 
 
 class TestPlan(Base):
     __tablename__ = "test_plan"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    description = Column(String)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    latency_enabled = Column(Boolean)
+    name = Column(String, nullable=False)
+    description = Column(String, default='')
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    latency_enabled = Column(Boolean, default=False)
     client_latency = Column(Integer)
     server_latency = Column(Integer)
     rules = relationship("Rule", backref="test_plan")
-
-
-class SessionExecution(Base):
-    __tablename__ = "session_execution"
-
-    id = Column(Integer, primary_key=True)
-    session = Column(Integer, ForeignKey('session.id'))
-    started_at = Column(DateTime)
-    stopped_at = Column(DateTime)
 
 
 class Rule(Base):
     __tablename__ = "rule"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
-    rule_type = Column(String)  # "request" or "response"
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    rule_type = Column(String, nullable=False)  # "request" or "response"
     filters = relationship("Filter")
     actions = relationship("Action")
-    test_plan = Column(Integer, ForeignKey('test_plan.id'))
+    test_plan = Column(Integer, ForeignKey('test_plan.id'), nullable=False)
 
 
 # TODO: Should we have a unified Filter table or 2 (request and response)?
@@ -103,11 +104,11 @@ class Filter(Base):
     __tablename__ = "filter"
 
     id = Column(Integer, primary_key=True)
-    method = Column(String, nullable=True)
-    status_code = Column(String, nullable=True)
-    url = Column(String, nullable=True)
-    protocol = Column(String, nullable=True)
-    rule = Column(Integer, ForeignKey('rule.id'))
+    method = Column(String)
+    status_code = Column(String)
+    url = Column(String)
+    protocol = Column(String)
+    rule = Column(Integer, ForeignKey('rule.id'), nullable=False)
 
 
 class FilterHeaders(Base):
@@ -122,8 +123,8 @@ class Action(Base):
     __tablename__ = "action"
 
     id = Column(Integer, primary_key=True)
-    type = Column(String)  # "request" or "response"
-    rule = Column(Integer, ForeignKey('rule.id'))
+    type = Column(String, nullable=False)  # "request" or "response"
+    rule = Column(Integer, ForeignKey('rule.id'), nullable=False)
 
 
 class ActionResponse(Action):
@@ -150,15 +151,14 @@ class ActionHeaders(Base):
     __tablename__ = "action_headers"
 
     id = Column(Integer, primary_key=True)
-    action = Column(Integer, ForeignKey('action.id'))
-    header = Column(Integer, ForeignKey('http_header.id'))
+    action = Column(Integer, ForeignKey('action.id'), nullable=False)
+    header = Column(Integer, ForeignKey('http_header.id'), nullable=False)
 
 
 class Match(Base):
     __tablename__ = "match"
 
     id = Column(Integer, primary_key=True)
-    session_execution = Column(Integer, ForeignKey('session_execution.id'))
-    rule = Column(Integer, ForeignKey('rule.id'))
-    http_request = Column(Integer, ForeignKey('http_request.id'))
-    created_at = Column(DateTime)
+    session = Column(Integer, ForeignKey('session.id'), nullable=False)
+    rule = Column(Integer, ForeignKey('rule.id'), nullable=False)
+    http_request = Column(Integer, ForeignKey('http_request.id'), nullable=False)
