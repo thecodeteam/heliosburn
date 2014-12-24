22c4cca87144f9377aaba2e112f834c7f85b4ef7
Moreno, Adrian <adrian@morenomartinez.com>
2014-12-24 12:27:32 +0100

    added sign in screen

--- heliosburn/django/hbproject/webui/urls.py
+++ heliosburn/django/hbproject/webui/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns(
     'webui.views',
     url(r'^$', 'dashboard', name='dashboard'),
+    url(r'^signin/$', 'signin', name='signin'),
     url(r'^sessions/$', 'session_manager', name='session_manager'),
     url(r'^sessions/(?P<id>[\w-]+)$', 'session_details', name='session_details'),
 )
--- heliosburn/django/hbproject/webui/views.py
+++ heliosburn/django/hbproject/webui/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def signin(request):
+    return render(request, 'signin.html')
+
+
 def dashboard(request):
     return render(request, 'dashboard.html')
 
