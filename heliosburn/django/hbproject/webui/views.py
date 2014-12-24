87289c545240f44dcb38ce29ea00611aee179cd5
Moreno, Adrian <adrian@morenomartinez.com>
2014-12-24 16:15:23 +0100

    test plan list logic

--- heliosburn/django/hbproject/webui/views.py
+++ heliosburn/django/hbproject/webui/views.py
@@ -18,7 +18,41 @@ def session_details(request):
 
 
 def testplan_list(request):
-    return render(request, 'testplan/list.html')
+    data = [
+        {
+            'id': 1,
+            'name': 'Amazon S3 Test Plan',
+            'description': 'My test plan for Amazon S3...',
+            'rules': 42,
+            'updated_at': '2014-02-12 03:34:51'
+        },
+        {
+            'id': 2,
+            'name': 'Test Plan for Swift',
+            'description': 'bla bla bla bla bla blaaaa...',
+            'rules': 654,
+            'updated_at': '2014-02-12 03:34:51'
+        },
+        {
+            'id': 3,
+            'name': 'EMC ViPR Test Plan',
+            'description': 'bla bla bla bla bla blaaaa...',
+            'rules': 34,
+            'updated_at': '2014-02-12 03:34:51'
+        },
+        {
+            'id': 4,
+            'name': 'Test Plan for EMC Atmos',
+            'description': 'bla bla bla bla bla blaaaa...',
+            'rules': 2134,
+            'updated_at': '2014-02-12 03:34:51'
+        },
+    ]
+
+    args = {}
+    args['data'] = data
+
+    return render(request, 'testplan/list.html', args)
 
 
 def testplan_details(request):
