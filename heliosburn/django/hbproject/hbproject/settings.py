46bf4f335e7b9a5d0a4a8b8fc0748f94a577a45b
hgrubbs <hgrubbs@cedarcreek.com>
2015-01-01 11:30:17 -0600

    pgsql configured for django session middleware
    gitignore updated

--- heliosburn/django/hbproject/hbproject/settings.py
+++ heliosburn/django/hbproject/hbproject/settings.py
@@ -59,8 +59,11 @@ WSGI_APPLICATION = 'hbproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'heliosburn',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
