f5001e2da2c2d475279764bb64ebf810043270f2
hgrubbs <hgrubbs@cedarcreek.com>
2015-01-02 11:04:36 -0600

    create/update stubs added to serializers

--- heliosburn/django/hbproject/api/serializers.py
+++ heliosburn/django/hbproject/api/serializers.py
@@ -12,15 +12,9 @@ class UserSerializer(serializers.Serializer):
     update_at = serializers.DateTimeField()
 
     def create(self, validated_data):
-        """
-        Create a new User.
-        """
         return User(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return existing User.
-        """
         pass
 
 
@@ -32,11 +26,23 @@ class HttpRequestSerializer(serializers.Serializer):
     url = serializers.CharField()
     response = serializers.CharField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class HttpRequestHeadersSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     request = serializers.IntegerField()
     header = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class HttpResponseSerializer(serializers.Serializer):
@@ -46,17 +52,35 @@ class HttpResponseSerializer(serializers.Serializer):
     status_code = serializers.IntegerField()
     status_description = serializers.CharField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class HttpResponseHeadersSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     response = serializers.IntegerField()
     header = serializers.IntegerField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class HttpHeaderSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     key = serializers.CharField()
     value = serializers.CharField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class SessionSerializer(serializers.Serializer):
@@ -70,11 +94,23 @@ class SessionSerializer(serializers.Serializer):
     started_at = serializers.DateTimeField()
     stopped_at = serializers.DateTimeField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class SessionTrafficSerializers(serializers.Serializer):
     id = serializers.IntegerField()
     session = serializers.IntegerField()
     http_request = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class TestPlanSerializer(serializers.Serializer):
@@ -88,6 +124,12 @@ class TestPlanSerializer(serializers.Serializer):
     server_latency = serializers.IntegerField()
     # 'rules' relationship TODO
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class RuleSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -98,6 +140,12 @@ class RuleSerializer(serializers.Serializer):
     # 'actions' relationsihp TODO
     test_plan = serializers.IntegerField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class FilterSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -107,17 +155,35 @@ class FilterSerializer(serializers.Serializer):
     protocol = serializers.CharField()
     rule = serializers.IntegerField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class FilterHeadersSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     filter = serializers.IntegerField()
     header = serializers.IntegerField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class ActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     type = serializers.CharField()
     rule = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class ActionResponseSerializer(serializers.Serializer):
@@ -127,6 +193,12 @@ class ActionResponseSerializer(serializers.Serializer):
     status_description = serializers.CharField()
     payload = serializers.CharField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class ActionRequestSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -135,11 +207,23 @@ class ActionRequestSerializer(serializers.Serializer):
     url = serializers.CharField()
     payload = serializers.CharField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class ActionHeadersSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.IntegerField()
     header = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class MatchSerializer(serializers.Serializer):
@@ -147,6 +231,12 @@ class MatchSerializer(serializers.Serializer):
     session = serializers.IntegerField()
     rule = serializers.IntegerField()
     http_request = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class RecordingSerializer(serializers.Serializer):
@@ -156,10 +246,22 @@ class RecordingSerializer(serializers.Serializer):
     user = serializers.IntegerField()
     created_at = serializers.DateTimeField()
 
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
 
 class RecordingTrafficSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     recording = serializers.IntegerField()
     http_request = serializers.IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
