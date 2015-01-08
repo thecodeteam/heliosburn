933f09b75bef5a456e13d101d4506d870011c7d2
Hunter Grubbs <hunter.grubbs@gmail.com>
2015-01-08 14:19:46 -0600

    RESTful stubs for session and test plan

--- heliosburn/django/hbproject/api/urls.py
+++ heliosburn/django/hbproject/api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
-from api.views import default,user
+from api.views import default, user, session, test_plan
 
 urlpatterns = patterns('',
     url(r'^$', default.index),
     url(r'^version$', default.version),
     url(r'^user$', user.rest),
+    url(r'^session$', session.rest),
+    url(r'^test_plan$', test_plan.rest),
 )
