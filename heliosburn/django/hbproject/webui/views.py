b16a9694b3def0728143a94e0310b52091d307a2
Moreno, Adrian <adrian@morenomartinez.com>
2015-01-08 16:37:03 +0100

    HS-20 more mock files and session execution upgrades

--- heliosburn/django/hbproject/webui/views.py
+++ heliosburn/django/hbproject/webui/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.templatetags.staticfiles import static
+import requests
+import json
+
+
+MOCK_PROTOCOL = "http"
+MOCK_HOST = "127.0.0.1"
+MOCK_PORT = "8000"
 
 WIZARD_STEPS = ['1', '2', '3', '4']
 
@@ -29,63 +37,10 @@ def session_new(request):
 
 
 def session_list(request):
-    sessions = [
-        {
-            'id': 1,
-            'name': 'Session A',
-            'testplan':
-                {
-                    'id': 12,
-                    'name': 'ViPR Test plan'
-                },
-            'executions': 42,
-            'latest_execution_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 2,
-            'name': 'ViPR Session 1',
-            'testplan':
-                {
-                    'id': 12,
-                    'name': 'ViPR Test plan'
-                },
-            'executions': 634,
-            'latest_execution_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 3,
-            'name': 'ViPR Session 2',
-            'testplan':
-                {
-                    'id': 12,
-                    'name': 'ViPR Test plan'
-                },
-            'executions': 341,
-            'latest_execution_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 4,
-            'name': 'Swift on-premise session',
-            'testplan':
-                {
-                    'id': 12,
-                    'name': 'Swift Test plan'
-                },
-            'executions': 654,
-            'latest_execution_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 5,
-            'name': 'S3 America session',
-            'testplan':
-                {
-                    'id': 12,
-                    'name': 'Amazon S3 Test plan'
-                },
-            'executions': 244,
-            'latest_execution_at': '2014-02-12 03:34:51'
-        }
-    ]
+
+    url = get_mock_url('session-list.json')
+    r = requests.get(url)
+    sessions = json.loads(r.text)
 
     args = {}
     args['sessions'] = sessions
@@ -94,63 +49,9 @@ def session_list(request):
 
 
 def session_details(request, id):
-    session = {
-        'id': 1,
-        'name': 'Session A',
-        'description': 'My session bla bla bla bla',
-        'created_at': '2014-02-12 03:34:51',
-        'updated_at': '2014-02-12 03:34:51',
-        'testplan':
-            {
-                'id': 12,
-                'name': 'Amazon S3 Test Plan',
-                'description': 'My test plan for Amazon S3...',
-                'rules': 42,
-                'updated_at': '2014-02-12 03:34:51'
-            },
-        'executions': [
-            {
-                'id': 1,
-                'date': '2014-02-12 03:34:51',
-                'duration': 39,
-                'requests': 48,
-                'testplan': {
-                    'id': 12,
-                    'name': 'Amazon S3 Test Plan'
-                }
-            },
-            {
-                'id': 4,
-                'date': '2014-02-12 03:34:51',
-                'duration': 432,
-                'requests': 74,
-                'testplan': {
-                    'id': 12,
-                    'name': 'Amazon S3 Test Plan'
-                }
-            },
-            {
-                'id': 5,
-                'date': '2014-02-12 03:34:51',
-                'duration': 123,
-                'requests': 324,
-                'testplan': {
-                    'id': 12,
-                    'name': 'Amazon S3 Test Plan'
-                }
-            },
-            {
-                'id': 12,
-                'date': '2014-02-12 03:34:51',
-                'duration': 75,
-                'requests': 436,
-                'testplan': {
-                    'id': 12,
-                    'name': 'Swift Test Plan'
-                }
-            }
-        ]
-    }
+    url = get_mock_url('session-details.json')
+    r = requests.get(url)
+    session = json.loads(r.text)
 
     args = {}
     args['session'] = session
@@ -159,21 +60,9 @@ def session_details(request, id):
 
 
 def session_execution(request, id):
-    session = {
-        'id': 1,
-        'name': 'Session A',
-        'description': 'My session bla bla bla bla',
-        'created_at': '2014-02-12 03:34:51',
-        'updated_at': '2014-02-12 03:34:51',
-        'testplan':
-            {
-                'id': 12,
-                'name': 'Amazon S3 Test Plan',
-                'description': 'My test plan for Amazon S3...',
-                'rules': 42,
-                'updated_at': '2014-02-12 03:34:51'
-            }
-        }
+    url = get_mock_url('session-details.json')
+    r = requests.get(url)
+    session = json.loads(r.text)
 
     args = {}
     args['session'] = session
@@ -198,36 +87,9 @@ def session_update(request):
 
 
 def testplan_list(request):
-    data = [
-        {
-            'id': 1,
-            'name': 'Amazon S3 Test Plan',
-            'description': 'My test plan for Amazon S3...',
-            'rules': 42,
-            'updated_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 2,
-            'name': 'Test Plan for Swift',
-            'description': 'bla bla bla bla bla blaaaa...',
-            'rules': 654,
-            'updated_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 3,
-            'name': 'EMC ViPR Test Plan',
-            'description': 'bla bla bla bla bla blaaaa...',
-            'rules': 34,
-            'updated_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 4,
-            'name': 'Test Plan for EMC Atmos',
-            'description': 'bla bla bla bla bla blaaaa...',
-            'rules': 2134,
-            'updated_at': '2014-02-12 03:34:51'
-        },
-    ]
+    url = get_mock_url('testplan-list.json')
+    r = requests.get(url)
+    data = json.loads(r.text)
 
     args = {}
     args['data'] = data
@@ -236,54 +98,9 @@ def testplan_list(request):
 
 
 def testplan_details(request, id):
-    testplan = {
-        'id': 4,
-        'name': 'Test Plan for EMC Atmos',
-        'description': 'bla bla bla bla bla blaaaa...',
-        'updated_at': '2014-02-12 03:34:51',
-        'created_at': '2014-02-12 03:34:51',
-        'latency_enabled': True,
-        'client_latency': 100,
-        'server_latency': 300,
-        'rules': [
-            {'id': 1,
-             'filter': 'GET, http://asdas.com/safg',
-             'action': 'New response',
-             'enabled': True,
-             'type': 'request'},
-            {'id': 2,
-             'filter': 'GET, http://asdas.com/safg',
-             'action': 'Modify header(X-Storage-URL)="blabla"',
-             'enabled': False,
-             'type': 'request'},
-            {'id': 3,
-             'filter': '200, Header="Hello"',
-             'action': 'New response',
-             'enabled': True,
-             'type': 'response'},
-            {'id': 4,
-             'filter': 'DELETE, http://asdas.com/safg',
-             'action': 'Drop connection',
-             'enabled': True,
-             'type': 'request'},
-            {'id': 5,
-             'filter': 'GET, http://asdas.com/safg',
-             'action': 'New response',
-             'enabled': True,
-             'type': 'request'},
-            {'id': 6,
-             'filter': 'GET, http://asdas.com/safg',
-             'action': 'New response',
-             'enabled': False,
-             'type': 'request'},
-            {'id': 7,
-             'filter': 'GET, http://asdas.com/safg',
-             'action': 'New response',
-             'enabled': True,
-             'type': 'request'},
-        ],
-
-    }
+    url = get_mock_url('testplan-details.json')
+    r = requests.get(url)
+    testplan = json.loads(r.text)
 
     args = {}
     args['testplan'] = testplan
@@ -313,11 +130,9 @@ def execution_details(request, id):
 
 
 def rule_details(request, id):
-    rule = {'id': 1,
-            'filter': 'GET, http://asdas.com/safg',
-            'action': 'New response',
-            'enabled': True,
-            'type': 'request'}
+    url = get_mock_url('rule-details.json')
+    r = requests.get(url)
+    rule = json.loads(r.text)
 
     args = {}
     args['rule'] = rule
@@ -326,57 +141,9 @@ def rule_details(request, id):
 
 
 def recording_list(request):
-    recordings = [
-        {
-            'id': 1,
-            'name': 'Recording 1',
-            'description': 'My recording bla bla bla...',
-            'requests': 42,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 2,
-            'name': 'Recording 2',
-            'description': 'My recording bla bla bla...',
-            'requests': 23,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 3,
-            'name': 'Recording 3',
-            'description': 'My recording bla bla bla...',
-            'requests': 2,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 4,
-            'name': 'Recording 4',
-            'description': 'My recording bla bla bla...',
-            'requests': 442,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 5,
-            'name': 'Recording 5',
-            'description': 'My recording bla bla bla...',
-            'requests': 345,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 6,
-            'name': 'Recording 6',
-            'description': 'My recording bla bla bla...',
-            'requests': 93,
-            'created_at': '2014-02-12 03:34:51'
-        },
-        {
-            'id': 7,
-            'name': 'Recording 7',
-            'description': 'My recording bla bla bla...',
-            'requests': 12,
-            'created_at': '2014-02-12 03:34:51'
-        },
-    ]
+    url = get_mock_url('recording-list.json')
+    r = requests.get(url)
+    recordings = json.loads(r.text)
 
     args = {}
     args['recordings'] = recordings
@@ -385,51 +152,9 @@ def recording_list(request):
 
 
 def recording_details(request, id):
-    recording = {
-        'id': 1,
-        'name': 'Recording 1',
-        'description': 'My recording bla bla bla...',
-        'created_at': '2014-02-12 03:34:51',
-        'duration': 343,
-        'traffic': [
-            {
-                'id': 23,
-                'method': 'GET',
-                'url': 'http://asdasdasd.com/ikdsf',
-                'date': '2014-02-12 03:34:51',
-                'response': {
-                    'status_code': 200
-                }
-            },
-            {
-                'id': 43,
-                'method': 'GET',
-                'url': 'http://asdasdasd.com/ikdsf',
-                'date': '2014-02-12 03:34:51',
-                'response': {
-                    'status_code': 200
-                }
-            },
-            {
-                'id': 54,
-                'method': 'GET',
-                'url': 'http://asdasdasd.com/ikdsf',
-                'date': '2014-02-12 03:34:51',
-                'response': {
-                    'status_code': 500
-                }
-            },
-            {
-                'id': 87,
-                'method': 'GET',
-                'url': 'http://asdasdasd.com/ikdsf',
-                'date': '2014-02-12 03:34:51',
-                'response': {
-                    'status_code': 200
-                }
-            }
-        ]
-    }
+    url = get_mock_url('recording-details.json')
+    r = requests.get(url)
+    recording = json.loads(r.text)
 
     args = {}
     args['recording'] = recording
@@ -455,3 +180,9 @@ def recording_update(request):
 
 def settings(request):
     return render(request, 'settings/settings.html')
+
+
+def get_mock_url(file):
+    static_path = static('mock/%s' % file)
+    url = "%s://%s:%s%s" % (MOCK_PROTOCOL, MOCK_HOST, MOCK_PORT, static_path)
+    return url
