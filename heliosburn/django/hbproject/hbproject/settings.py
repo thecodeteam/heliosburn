3b48774bf1058d80dcdd3b2b5712253d62a27d06
Moreno, Adrian <adrian@morenomartinez.com>
2015-01-09 12:33:55 +0100

    fixed sqlalchemy models. added backrefs.
    pulling in model changes from master

--- heliosburn/django/hbproject/hbproject/settings.py
+++ heliosburn/django/hbproject/hbproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
 #    'django.contrib.admin',
-#    'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
