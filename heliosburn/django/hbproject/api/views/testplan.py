b121614b70b5e18274609041ae0afb5bc3dcb58f
hgrubbs <hgrubbs@cedarcreek.com>
2015-01-22 09:17:33 -0600

    testplan PUT updated

--- heliosburn/django/hbproject/api/views/testplan.py
+++ heliosburn/django/hbproject/api/views/testplan.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from api.models import db_model
 from api.models.auth import RequireLogin
+from sqlalchemy.exc import IntegrityError
 import json
 
 
@@ -81,12 +82,73 @@ def get_all_testplans():
 
 def post(request):
     """Create a new test plan."""
-    pass
+    try:
+        new = json.loads(request.body)
+        assert "name" in new
+    except ValueError:
+        r = JsonResponse({"error": "invalid JSON"})
+        r.status_code = 400
+        return r
+    except AssertionError:
+        r = JsonResponse({"error": "argument mismatch"})
+        r.status_code = 400
+        return r
+
+    dbsession = db_model.init_db()
+    testplan = db_model.TestPlan(name=new['name'])
+    if "description" in new:
+        testplan.description = new['description']
+    if "latency_enabled" in new:
+        testplan.latency_enabled = new['latency_enabled']
+    if "client_latency" in new:
+        testplan.client_latency = new['client_latency']
+    if "server_latency" in new:
+        testplan.server_latency = new['server_latency']
+
+    dbsession.add(testplan)
+    dbsession.commit()
+    r = JsonResponse({})
+    r.status_code = 204
+    return r
+
 
 def put(request, testplan_id):
     """Update existing test plan."""
-    pass
+    try:
+        in_json = json.loads(request.body)
+    except ValueError:
+        r = JsonResponse({"error": "invalid JSON"})
+        r.status_code = 400
+        return r
+
+    dbsession = db_model.init_db()
+    testplan = db_model.query(db_model.TestPlan).filter_by(id=testplan_id).first()
+    if testplan is None:
+        r = JsonResponse({})
+        r.status_code = 404
+        return r
+    else:
+        if "name" in in_json:
+            testplan.name = in_json['name']
+        if "description" in in_json:
+            testplan.description = in_json['description']
+        if "latency_enabled" in in_json:
+            testplan.latency_enabled = in_json['latency_enabled']
+        if "client_latency" in in_json:
+            testplan.client_latency = in_json['client_latency']
+        if "server_latency" in in_json:
+            testplan.server_latency = in_json['server_latency']
+        try:
+            dbsession.commit()
+        except IntegrityError:
+            r = JsonResponse({"error": "constraint violated"})
+            r.status_code = 409
+            return r
+        r = JsonResponse({})
+        r.status_code = 204
+        return r
+
 
 def delete(request, testplan_id):
     """Delete existing test plan."""
-    pass
+
