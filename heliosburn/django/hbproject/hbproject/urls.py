5aa1a7012a25aa8ce03b39ba8805671f185fcb4f
Moreno, Adrian <adrian@morenomartinez.com>
2015-01-22 11:10:19 +0100

    added root url redirection to webui login page

--- heliosburn/django/hbproject/hbproject/urls.py
+++ heliosburn/django/hbproject/hbproject/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -7,6 +8,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', RedirectView.as_view(url='/webui', permanent=False), name='index'),
     url(r'^api/', include('api.urls')),
     url(r'^webui/', include('webui.urls')),
 )
