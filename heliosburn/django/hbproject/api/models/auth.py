4db179dad3aba5944595bd8137b96b4defbe83d9
hgrubbs <hgrubbs@cedarcreek.com>
2015-01-22 10:08:36 -0600

    sessions will pull user_id from incoming request. @RequireLogin modified to inject user_id into request

--- heliosburn/django/hbproject/api/models/auth.py
+++ heliosburn/django/hbproject/api/models/auth.py
@@ -3,31 +3,25 @@ class RequireLogin(object):
     def __init__(self, f):
         self.f = f
         self.token_string = "INVALID"  # valid string that will always fail to validate
+        self.user_id = None
 
     def __call__(self, request, *pargs, **kwargs):
         if 'HTTP_X_AUTH_TOKEN' in request.META:
             self.token_string = request.environ['HTTP_X_AUTH_TOKEN']
-            if self.valid_token_redis():
+            if self.valid_token():
+                request.user_id = self.user_id
                 return self.f(request)
 
         # 401 Unauthorized if you reach this point
         from django.http import HttpResponse
         return HttpResponse(status=401)
 
-    def valid_token(self):  # TODO: DEPRECATED, remove this once redis is 100% confirmed for tokens
-        import db_model
-        dbsession = db_model.init_db()
-        user = dbsession.query(db_model.User).filter_by(token=self.token_string).first()
-        if user is None:
-            return False
-        else:
-            return True
-
-    def valid_token_redis(self):
+    def valid_token(self):
         from api.models import redis_wrapper
         r = redis_wrapper.init_redis(1)
         r = r.get(self.token_string)
         if r is None:
             return False
         else:
+            self.user_id = int(r)
             return True
