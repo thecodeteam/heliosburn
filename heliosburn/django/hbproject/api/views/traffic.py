9aad521e1e72c0531020cb9239d917325ba40a2f
hgrubbs <hgrubbs@cedarcreek.com>
2015-01-26 10:31:15 -0600

    docstrings added where missing

--- heliosburn/django/hbproject/api/views/traffic.py
+++ heliosburn/django/hbproject/api/views/traffic.py
@@ -31,6 +31,7 @@ def rest(request, *pargs):
 
 @RequireLogin
 def get(request):
+    """Retrieve traffic from redis backend"""
     r = redis_wrapper.init_redis()
 
     key = 'token:%s' % (request.token_string,)
