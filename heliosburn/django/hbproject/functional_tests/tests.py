40036de52ccb7c384edc9b4a10181c1f6b90a485
Moreno, Adrian <adrian@morenomartinez.com>
2015-02-03 12:05:15 +0100

    added functional tests file (no tests yet)

---
+++ heliosburn/django/hbproject/functional_tests/tests.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+
+
+# class NewVisitorTest(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.browser = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+#            desired_capabilities=DesiredCapabilities.CHROME)
+#         self.browser.implicitly_wait(3)
+#
+#     def tearDown(self):
+#         self.browser.quit()
+#         pass
+#
+#     def test_user_signs_in(self):
+#         self.browser.get('http://localhost:8000')
+#         pass
+
+
+if __name__ == '__main__':
+    #unittest.main()
+    pass
