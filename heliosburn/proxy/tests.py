a8ff0273e72c76334c0e1106bfeb5e74d4dfd554
hgrubbs <hgrubbs@cedarcreek.com>
2015-02-04 16:02:15 -0600

    working unit test with testserver(threaded) and proxycore(seperate process) integrated. running the test will spawn and tear down the testserver and proxycore components respectively

--- heliosburn/proxy/tests.py
+++ heliosburn/proxy/tests.py
@@ -1,9 +1,11 @@
 import unittest
 from threading import Thread
+import subprocess
 import time
 import testserver
 import urllib2
 import logging
+import proxy_core
 
 
 class TestServer(Thread):
@@ -20,15 +22,18 @@ class ProxyCoreTest(unittest.TestCase):
         time.sleep(1)  # Let cherrypy engine thread start and begin listening
 
         logging.warning("Starting proxy_core")
+        self.proxy_core_process = subprocess.Popen(["/usr/bin/python2.7", "proxy_core.py"])  # TODO: fix this, obviously :)
+        time.sleep(1)  # Let twisted factory begin listening
 
     def tearDown(self):
         logging.warning("Shutting down TestServer gracefully")
         time.sleep(1)  # Let cherrypy engine be idle before sending /die
         urllib2.urlopen("http://127.0.0.1:8080/die")
         time.sleep(1)  # Let cherrypy thread exit cleanly
-        assert self.testserver_thread.is_alive() is False
+
+        self.proxy_core_process.kill()
 
     def test_proxy_logs_200(self):
         logging.warning("Testing logging of HTTP 200")
-        x = urllib2.urlopen("http://127.0.0.1:8080")
+        x = urllib2.urlopen("http://127.0.0.1:8880")
         pass
