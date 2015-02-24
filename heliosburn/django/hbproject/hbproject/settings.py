e21ec9f058f71dbdc305612941cae74e806ec85b
Moreno, Adrian <adrian@morenomartinez.com>
2015-02-24 12:18:18 +0100

    added multiple deployment environment settings with django-dotenv and django-configuration

--- heliosburn/django/hbproject/hbproject/settings.py
+++ heliosburn/django/hbproject/hbproject/settings.py
@@ -12,129 +12,160 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 from django.contrib.messages import constants as message_constants
 import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_v2kchanhwkl&syq$d8*a3&9po-@76m@jud9*gm8dd9zf0be*+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+from configurations import Configuration, values
 
 
-# Application definition
+class Common(Configuration):
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-    'django_bootstrap_breadcrumbs',
-    'bootstrap3',
-    'debug_toolbar',
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-    'webui',
-)
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = values.SecretValue()
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = values.BooleanValue(False)
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages',
-)
+    TEMPLATE_DEBUG = values.BooleanValue(DEBUG)
 
-ROOT_URLCONF = 'hbproject.urls'
-
-WSGI_APPLICATION = 'hbproject.wsgi.application'
+    ALLOWED_HOSTS = []
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+    # Application definition
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'heliosburn',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+
+        'django_bootstrap_breadcrumbs',
+        'bootstrap3',
+
+        'webui',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        'django.core.context_processors.request',
+        'django.contrib.messages.context_processors.messages',
+    )
+
+    ROOT_URLCONF = 'hbproject.urls'
+
+    WSGI_APPLICATION = 'hbproject.wsgi.application'
+
+
+    # Database
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'heliosburn',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-
-LOGIN_URL = '/webui/signin'
-
-AUTHENTICATION_BACKENDS = (
-    'webui.backends.HeliosAuthBackend',
-)
-
-API_BASE_URL = 'http://127.0.0.1:8000/api'
+    USE_TZ = True
 
 
-TOKEN_TTL = 3600  # milliseconds
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 0
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+    LOGIN_URL = '/webui/signin'
+
+    AUTHENTICATION_BACKENDS = (
+        'webui.backends.HeliosAuthBackend',
+    )
+
+    API_BASE_URL = 'http://127.0.0.1:8000/api'
 
 
-# Workaround to adapt the message levels to Bootstrap css styles
-MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
-                message_constants.INFO: 'info',
-                message_constants.SUCCESS: 'success',
-                message_constants.WARNING: 'warning',
-                message_constants.ERROR: 'danger', }
+    TOKEN_TTL = 3600  # milliseconds
+
+    REDIS_HOST = 'localhost'
+    REDIS_PORT = 6379
+    REDIS_DB = 0
+
+    # Workaround to adapt the message levels to Bootstrap css styles
+    MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
+                    message_constants.INFO: 'info',
+                    message_constants.SUCCESS: 'success',
+                    message_constants.WARNING: 'warning',
+                    message_constants.ERROR: 'danger', }
+
+    MONGODB_HOST = 'localhost'
+    MONGODB_PORT = 27017
+    MONGODB_DATABASE = {
+        'production': 'heliosburn',  # for normal operation
+        'test': 'heliosburn_test'  # for unit testing
+    }
+
+
+class Development(Common):
+    """
+    The in-development settings and the default configuration.
+    """
+    DEBUG = True
+
+    TEMPLATE_DEBUG = True
+
+    ALLOWED_HOSTS = []
+
+    INSTALLED_APPS = Common.INSTALLED_APPS + (
+        'debug_toolbar',
+    )
+
+    # Django-Debug-Toolbar workaround to make it work with the Vagrant VM
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': "%s.true" % __name__,
+    }
+
+
+class Staging(Common):
+    """
+    The in-staging settings.
+    """
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+
+class Production(Staging):
+    """
+    The in-production settings.
+    """
+    pass
+
 
 # Django-Debug-Toolbar workaround to make it work with the Vagrant VM
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': "%s.true" % __name__,
-}
-
-
 def true(request):
-    return True
-
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-MONGODB_DATABASE = {
-    'production': 'heliosburn',  # for normal operation
-    'test': 'heliosburn_test'  # for unit testing
-}
+        return True
