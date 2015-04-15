36eccd208695219d36d40b2542083871aa7b0ba0
Moreno, Adrian <adrian@morenomartinez.com>
2015-04-15 17:16:59 +0200

    fixes some webui logs

--- heliosburn/django/hbproject/webui/views/login.py
+++ heliosburn/django/hbproject/webui/views/login.py
@@ -26,20 +26,20 @@ class LoginView(View):
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
         try:
-            logger.info('Authentication request for the user %s', username)
+            logger.info("Authentication request for the user %s" % (username, ))
             user = auth.authenticate(username=username, password=password)
         except Exception as inst:
-            logger.error('Something went wrong while authenticating the user %s', username, exc_info=True)
+            logger.error("Something went wrong while authenticating the user %s" % (username, ), exc_info=True)
             messages.error(request, 'Something went wrong. %s' % (inst,))
             return render(request, self.template_name, {'form': form})
 
         if not user:
-            logger.info('Could not authenticate the user %s. Bad credentials.', username)
+            logger.info("Could not authenticate the user %s. Bad credentials." % (username, ))
             messages.error(request, 'Invalid login credentials')
             return render(request, self.template_name, {'form': form})
 
         auth.login(request, user)
         redirect_url = request.GET.get('next', reverse('dashboard'))
 
-        logger.info('User %s authenticated successfully', username)
+        logger.info("User %s authenticated successfully" % (username, ))
         return HttpResponseRedirect(redirect_url)
