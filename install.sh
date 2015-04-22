9290adc0cecfcc9a0edbc2e9bfb255c01192c9b8
hgrubbs <hunter.grubbs@gmail.com>
2015-04-22 16:43:28 -0500

    requirements.txt set to use stable versions of packages

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-install -b heliosburn/supervisord_conf_d/*.conf /etc/supervisor/conf.d/
+if [ -d /opt/HeliosBurn ]
+then
+    install -b heliosburn/supervisord_conf_d/*.conf /etc/supervisor/conf.d/
+    exit 0
+else
+    echo "'/opt/HeliosBurn' does not exist! Ensure HeliosBurn is decompressed into /opt/ and re-run this."
+    exit 1
+fi
