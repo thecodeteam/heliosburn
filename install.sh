1d9036588dc4bf5ec308a6dc3dcfbf07c0cc5579
hgrubbs <hunter.grubbs@gmail.com>
2015-04-22 17:00:13 -0500

    install script modified to check for root

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+if [ "$EUID" != "0" ]
+then
+    echo "You must be the root user to run this installer."
+    exit 1
+fi
+
 if [ -d /opt/HeliosBurn ]
 then
+    echo ">> Installing supervisord profiles under /etc/supervisor/conf.d/"
     install -b heliosburn/supervisord_conf_d/*.conf /etc/supervisor/conf.d/
+    echo ">> Setting up database - executing: 'python heliosburn/django/hbproject/create_db_model.py'"
+    python heliosburn/django/hbproject/create_db_model.py
+    echo ""
+    echo "Installation complete - restart supervisord to load and run the Helios Burn components."
     exit 0
 else
     echo "'/opt/HeliosBurn' does not exist! Ensure HeliosBurn is decompressed into /opt/ and re-run this."
     exit 1
 fi
+
