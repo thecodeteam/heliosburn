8eae710a925b519dde3a7147442b0d50efd074fc
Hunter Grubbs <hunter.grubbs@gmail.com>
2015-06-29 11:15:37 -0500

    changes to match new api

--- heliosburn/hbcli/controllers/proxy.py
+++ heliosburn/hbcli/controllers/proxy.py
@@ -34,6 +34,32 @@ def traffic(config, args):
         pp.pprint(json.loads(r.content))
 
 
+def start(config, args):
+    pp = pprint.PrettyPrinter()
+    url = config['url'] + "/api/proxy/start/"
+
+    token = auth.get_token(config)
+    r = requests.post(url, headers={"X-Auth-Token": token})
+    if r.status_code != 200:
+        print("API returned status code %s" % (r.status_code))
+        sys.exit(1)
+    else:
+        pp.pprint(json.loads(r.content))
+
+
+def stop(config, args):
+    pp = pprint.PrettyPrinter()
+    url = config['url'] + "/api/proxy/stop/"
+
+    token = auth.get_token(config)
+    r = requests.post(url, headers={"X-Auth-Token": token})
+    if r.status_code != 200:
+        print("API returned status code %s" % (r.status_code))
+        sys.exit(1)
+    else:
+        pp.pprint(json.loads(r.content))
+
+
 def main(config, args):
     description = "Interact with the proxy."
     m = re.match(r'^.*\.(.*)$', __name__)
@@ -48,8 +74,16 @@ def main(config, args):
     # status
     traffic_parser = subparsers.add_parser("traffic", help="read proxy traffic")
 
+    # start
+    traffic_parser = subparsers.add_parser("start", help="stop proxy")
+
+    # stop
+    traffic_parser = subparsers.add_parser("stop", help="start proxy")
+
     args = vars(parser.parse_args())
     action_map = {
+        "start": start,
+        "stop": stop,
         "status": status,
         "traffic": traffic,
     }
