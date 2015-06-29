6f8871a43c47dbd5e1fb411dfb42cf37595d2dfb
Hunter Grubbs <hunter.grubbs@gmail.com>
2015-06-29 10:42:19 -0500

    traffic GET added to cli->proxy

--- heliosburn/hbcli/controllers/proxy.py
+++ heliosburn/hbcli/controllers/proxy.py
@@ -21,6 +21,19 @@ def status(config, args):
         pp.pprint(json.loads(r.content))
 
 
+def traffic(config, args):
+    pp = pprint.PrettyPrinter()
+    url = config['url'] + "/api/traffic/"
+
+    token = auth.get_token(config)
+    r = requests.get(url, headers={"X-Auth-Token": token})
+    if r.status_code != 200:
+        print("API returned status code %s" % (r.status_code))
+        sys.exit(1)
+    else:
+        pp.pprint(json.loads(r.content))
+
+
 def main(config, args):
     description = "Interact with the proxy."
     m = re.match(r'^.*\.(.*)$', __name__)
@@ -32,8 +45,12 @@ def main(config, args):
     # status
     read_parser = subparsers.add_parser("status", help="read proxy status")
 
+    # status
+    traffic_parser = subparsers.add_parser("traffic", help="read proxy traffic")
+
     args = vars(parser.parse_args())
     action_map = {
         "status": status,
+        "traffic": traffic,
     }
     action_map[args['action']](config, args)
