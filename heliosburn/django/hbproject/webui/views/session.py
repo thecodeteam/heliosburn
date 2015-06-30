ef9df25ea26539d6765d38e641ffd06ce1c30157
Moreno, Adrian <adrian@morenomartinez.com>
2015-06-30 16:22:00 +0200

    Fixed start and stop Session from WebUI

--- heliosburn/django/hbproject/webui/views/session.py
+++ heliosburn/django/hbproject/webui/views/session.py
@@ -5,11 +5,12 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 import requests
 
-from webui.exceptions import UnauthorizedException, NotFoundException
+from webui.exceptions import UnauthorizedException, NotFoundException, ServerErrorException
 from webui.models import Session
 from webui.views import signout, get_mock_url
 
@@ -73,9 +74,11 @@ def session_details(request, session_id):
 
 @login_required
 def session_execute(request, session_id):
+
     try:
-        Session(auth_token=request.user.password).start(session_id)
         session = Session(auth_token=request.user.password).get(session_id)
+        data = {'session': session}
+        return render(request, 'sessions/session_execution.html', data)
     except UnauthorizedException:
         logger.warning('User unauthorized. Signing out...')
         return signout(request)
@@ -86,9 +89,6 @@ def session_execute(request, session_id):
         logger.error('Unexpected exception', exc_info=True)
         messages.error(request, inst.message if inst.message else 'Unexpected error')
         return HttpResponseRedirect(reverse('session_details', args=(str(session_id),)))
-
-    data = {'session': session}
-    return render(request, 'sessions/session_execution.html', data)
 
 
 @login_required
@@ -106,3 +106,54 @@ def session_update(request):
     else:
         # TODO: API call to update value
         return HttpResponse()
+
+
+@login_required
+@csrf_exempt
+def session_start(request, session_id):
+    if request.method != 'POST':
+        return HttpResponseBadRequest('Method must be POST')
+
+    try:
+        Session(auth_token=request.user.password).start(session_id)
+    except UnauthorizedException:
+        logger.warning('User unauthorized. Signing out...')
+        signout(request)
+        return HttpResponseBadRequest('Unauthorized')
+    except NotFoundException:
+        logger.warning('The requested Session "%s" does not exist', session_id)
+        return HttpResponseBadRequest('Resource not found')
+    except ServerErrorException:
+        logger.error('Unexpected exception in the API', exc_info=True)
+        return HttpResponseServerError()
+    except Exception as inst:
+        logger.error('Unexpected exception', exc_info=True)
+        message = inst.message if inst.message else 'Unexpected error'
+        return HttpResponseBadRequest(message)
+    return HttpResponse('started')
+
+
+@login_required
+@csrf_exempt
+def session_stop(request, session_id):
+    if request.method != 'POST':
+        return HttpResponseBadRequest('Method must be POST')
+
+    try:
+        Session(auth_token=request.user.password).stop(session_id)
+    except UnauthorizedException:
+        logger.warning('User unauthorized. Signing out...')
+        signout(request)
+        return HttpResponseBadRequest('Unauthorized')
+    except NotFoundException:
+        logger.warning('The requested Session "%s" does not exist', session_id)
+        return HttpResponseBadRequest('Resource not found')
+    except ServerErrorException:
+        logger.error('Unexpected exception in the API', exc_info=True)
+        return HttpResponseServerError()
+    except Exception as inst:
+        logger.error('Unexpected exception', exc_info=True)
+        message = inst.message if inst.message else 'Unexpected error'
+        return HttpResponseBadRequest(message)
+
+    return HttpResponse('stopped')
